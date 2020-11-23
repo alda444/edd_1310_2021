5d07b0074ab58b51acf41405c7b528c696ac53f7
alda444 <61130360+alda444@users.noreply.github.com>
2020-11-23 17:00:24 -0600

    Add files via upload

---
+++ Tarea 6/Pruebas_listas_ligadas.py
@@ -0,0 +1,18 @@
+l = LinkedList()
+print(f"La esta vacia ? { l.is_empty() } ")
+l.append(10)
+l.append(5)
+l.append(6)
+l.append(20)
+print(f"La esta vacia ? { l.is_empty() } ")
+l.transversal()
+l.remove(10)
+l.transversal()
+l.preppend(3)
+l.transversal()
+
+x = l.tail()
+
+print(x.data)
+
+print(l.get(2))
