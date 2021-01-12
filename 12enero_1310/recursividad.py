2e7d700dacf9d2e05684c1d8c50431c681ba5f0d
alda444 <61130360+alda444@users.noreply.github.com>
2021-01-12 12:49:19 -0600

    Add files via upload

---
+++ 12enero_1310/recursividad.py
@@ -0,0 +1,36 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return factorial(n-1) * n
+
+def printRev(n):
+    if n > 0:
+        #print(n)
+        printRev(n-1)
+        print(n)
+
+def fibonacci(n):
+    if n == 1 or n == 0:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+def main():
+    for num in range(1,11,1):
+        r = factorial(num)
+        print(f"El factorial de {num} es {r}")
+
+def main2():
+    printRev(3)
+
+def main3():
+    for num in range(11):
+        print(str(fibonacci(num))+"," , end = "")
+    print("")
+
+main3()
+
+main2()
+
+main()
